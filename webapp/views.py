72ef811148e97fe1c69ec0624c36cabe0ce6ece0
Alfonso Corona <coronaalfonsor@gmail.com>
2024-12-28 13:46:34 -0600

    feat: completing register page

--- webapp/views.py
+++ webapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
   if request.method == 'POST':
     username = request.POST['username']
     password = request.POST['password']
-
+    print(username, password)
     user = authenticate(request, username=username, password=password)
+    print(user)
 
     if user is not None:
       login(request, user)
@@ -24,4 +26,27 @@ def login_user(request):
   pass
 
 def logout_user(request):
-  pass
+  logout(request)
+  messages.success(request, 'You have successfully logged out!')
+  return redirect('home')
+
+def register_user(request):
+  if(request.method == 'POST'):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password1']
+
+      user = authenticate(username=username, password=password)
+      login(request, user)
+      messages.success(request, 'You have successfully registered!')
+      return redirect('home')
+  else:
+    form = SignUpForm()
+    return render(request, 'register.html', {
+      'form': form
+    })
+  return render(request, 'register.html', {
+    'form': form
+  })
